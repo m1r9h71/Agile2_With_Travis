673c5c73ab6890784f4c3ef381be7408b59721f1
Matthew Hoing <mattatwit15@gmail.com>
2017-11-02 14:37:18 +0000

    Get all people endpoint tested

--- test/routes/person-test.js
+++ test/routes/person-test.js
@@ -4,8 +4,40 @@ var server = require('../../bin/www');
 var expect = chai.expect;
 
 chai.use(chaiHttp);
-var_=require('lodash');
-
+var _ = require('lodash' );
+chai.use(require('chai-things'));
 describe('Person', function(){
-    //TODO
+    describe ('GET /people', function() {
+        it('should return all the people in an array', function(done){
+            chai.request(server)
+                .get('/people')
+                .end(function(err, res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(5);
+                    var result = _.map(res.body, function(people){
+
+                        /*return {id: people.id,
+                            fname : people.fname,
+                            lname : people.lname,
+                            age : people.age,
+                            gender : people.gender,
+                            address : people.address}*/
+                        return {_id: people._id,
+                            address : people.address,
+                            gender : people.gender,
+                            age : people.age,
+                            lname : people.lname,
+                            fname : people.fname}
+                    });
+
+                    expect(result).to.include({"_id":"59d2c6d998773510b8bebfbd","address":"Ballybricken, Waterford","gender":"male","age":46,"lname":"Hoing","fname":"Matt"});
+                    expect(result).to.include({"_id":"59d2c748b4cdc81618b5d73e","address":"Ballybricken, Waterford","gender":"female","age":23,"lname":"Coughlan","fname":"Elaine"});
+                    expect(result).to.include({"_id":"59d2c93039f6503004439f9e","address":"Ballybricken, Waterford","gender":"female","age":21,"lname":"Lacoste","fname":"Catherine"});
+                    expect(result).to.include({"_id":"59d2c96439f6503004439f9f","address":"Ballytruckle Road, Waterford","gender":"male","age":47,"lname":"Hoing","fname":"Ian"});
+                    expect(result).to.include({"_id":"59d2c99539f6503004439fa0","address":"Updated Test Data","gender":"male","age":19,"lname":"Updated Data","fname":"Updated Test"});
+                    done();
+                });
+        });
+    });
 });
