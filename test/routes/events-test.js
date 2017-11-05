97a4a36eb601337506cb4588626d302dbe56c764
Matthew Hoing <mattatwit15@gmail.com>
2017-11-05 17:06:03 +0000

    get all events endpoint tested

--- test/routes/events-test.js
+++ test/routes/events-test.js
@@ -29,4 +29,28 @@ describe('Event', function() {
                 });
         });
     });
+
+    describe ('GET /events', function() {
+        it('should return all the events in the array', function(done){
+            chai.request(server)
+            .get('/events')
+                .end(function(err, res){
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    var result = _.map(res.body, function(events){
+                        return {_id: events._id,
+                        title : events.title,
+                        description : events.description,
+                        place : events.place,
+                        startdate : events.startdate,
+                        finishdate : events.finishdate,
+                        status : events.status}
+                    });
+                     expect(result).to.include({"_id":"59cc1782102ff512a46ea446","status":"c","finishdate":"24/10/2017","startdate":"23/10/2017","place":"IT119","description":"TEst Edit All","title":"Test Edit All"}),
+                     expect(result).to.include({"_id":"59ecee77e2756213d0d08d02","status":"c","finishdate":"24/10/2017","startdate":"23/10/2017","place":"IT119","description":"Demo 30% assignment","title":"Assignment Demo"}),
+                     expect(result).to.include({"_id":"59ee04247cebeb2b980f1f19","status":"c","finishdate":"24/10/2017","startdate":"23/10/2017","place":"IT119","description":"Demo 30% assignment3","title":"Assignment Demo44"});
+                     done();
+                });
+        });
+    });
 });
